ef3f9ce20775d013bb9ce033c2f7ac39d9f64b60
chessarose1 <84988168+chessarose1@users.noreply.github.com>
2021-07-24 16:32:51 -0400

    Update Code.py

--- Code.py
+++ Code.py
@@ -1,1 +1,3 @@
-print("hello”
+print("Buzzfeed Quiz: Tell Us Your Style & We will Tell You What Subject You’ll Teach”)
+question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and Blouse \n C. Romper \n D. Whatever is Clean”)
+qustion2 = input("When lounging around the house what do wear: \n A. T-shirt and Sweatpants /n B. Nothing \n C. Bathing Suit \n D. Pajamas”)
