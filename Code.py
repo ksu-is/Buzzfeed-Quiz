23220461923a3092fa45916d424ee887b92ab6e8
chessarose1 <cbirrell@students.kennesaw.edu>
2021-07-26 19:06:45 -0400

    Update Code.py
    Added a question for the quiz for user to answer

--- Code.py
+++ Code.py
@@ -4,4 +4,5 @@ question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and B
 question2 = input("When lounging around the house what do you wear: \n A. T-shirt and Sweatpants /n B. Nothing \n C. Bathing Suit \n D. Pajamas")
 question3 = input("What are your go to shoes: \n A. Louboutins \n B. Nike \n C. Crocks \n D. UGGs")
 question4 = input("WWhat is your favorite color scheme: \n A. Bright Summer Colors \n B. Black?/White \n C. Fall Colors \n D. Natural Colors ")
-question5 = input("What is your typical makeup routine: A. All Glam B. Natural Look C. Bare Face D. Only Special Occasions")
+question5 = input("What is your typical makeup routine: \n A. All Glam \n B. Natural Look \n C. Bare Face \n D. Only Special Occasions")
+question6 = input("What’s favorite type of jeans: \n A. Skinny Jeans \n B. Bootcut Jeans \n C. Boyfriend Cut Jeans \n D. Jean Shorts")
