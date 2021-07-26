22111a9e4fa86b68e04776acb8968afb839b2534
chessarose1 <cbirrell@students.kennesaw.edu>
2021-07-26 18:52:35 -0400

    Update Code.py
    Added a new question for quiz

--- Code.py
+++ Code.py
@@ -1,4 +1,6 @@
 print("Buzzfeed Quiz: Tell Us Your Style & We will Tell You What Subject You’ll Teach")
+#Questions the quiz will ask: 
 question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and Blouse \n C. Romper \n D. Whatever is Clean")
-qustion2 = input("When lounging around the house what do you wear: \n A. T-shirt and Sweatpants /n B. Nothing \n C. Bathing Suit \n D. Pajamas")
+question2 = input("When lounging around the house what do you wear: \n A. T-shirt and Sweatpants /n B. Nothing \n C. Bathing Suit \n D. Pajamas")
 question3 = input("What are your go to shoes: \n A. Louboutins \n B. Nike \n C. Crocks \n D. UGGs")
+question4 = input("WWhat is your favorite color scheme: \n A. Bright Summer Colors \n B. Black?/White \n C. Fall Colors \n D. Natural Colors ")
