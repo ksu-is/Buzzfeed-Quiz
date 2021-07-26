2f0c0b5be72ec5b231bd84b07e76617f1d57a783
chessarose1 <cbirrell@students.kennesaw.edu>
2021-07-26 18:41:35 -0400

    Update Code.py
    Added Questions for quiz

--- Code.py
+++ Code.py
@@ -1,4 +1,4 @@
-print("Buzzfeed Quiz: Tell Us Your Style & We will Tell You What Subject You’ll Teach”)
-question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and Blouse \n C. Romper \n D. Whatever is Clean”)
-qustion2 = input("When lounging around the house what do you wear: \n A. T-shirt and Sweatpants /n B. Nothing \n C. Bathing Suit \n D. Pajamas”)
-question3 = input("What are your go to shoes: \n A. Louboutins \n B. Nike \n C. Crocks \n D. UGGs”)
+print("Buzzfeed Quiz: Tell Us Your Style & We will Tell You What Subject You’ll Teach")
+question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and Blouse \n C. Romper \n D. Whatever is Clean")
+qustion2 = input("When lounging around the house what do you wear: \n A. T-shirt and Sweatpants /n B. Nothing \n C. Bathing Suit \n D. Pajamas")
+question3 = input("What are your go to shoes: \n A. Louboutins \n B. Nike \n C. Crocks \n D. UGGs")
