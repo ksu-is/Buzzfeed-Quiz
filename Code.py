7d20a84fffa6b62198e4c37aa7d362c31144b6e3
chessarose1 <cbirrell@students.kennesaw.edu>
2021-07-27 17:14:16 -0400

    Update Code.py
    Add more if statements and quiz function to run quiz. Also added while statement

--- Code.py
+++ Code.py
@@ -1,17 +1,149 @@
 print("Buzzfeed Quiz: Tell Us Your Style & We will Tell You What Subject You’ll Teach")
+print("Please Put Your Answer As a Capital Letter")
 #Questions the quiz will ask: 
-question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and Blouse \n C. Romper \n D. Whatever is Clean")
-question2 = input("When lounging around the house what do you wear: \n A. T-shirt and Sweatpants /n B. Nothing \n C. Bathing Suit \n D. Pajamas")
-question3 = input("What are your go to shoes: \n A. Louboutins \n B. Nike \n C. Crocks \n D. UGGs")
-question4 = input("WWhat is your favorite color scheme: \n A. Bright Summer Colors \n B. Black?/White \n C. Fall Colors \n D. Natural Colors ")
-question5 = input("What is your typical makeup routine: \n A. All Glam \n B. Natural Look \n C. Bare Face \n D. Only Special Occasions")
-question6 = input("What’s favorite type of jeans: \n A. Skinny Jeans \n B. Bootcut Jeans \n C. Boyfriend Cut Jeans \n D. Jean Shorts")
+question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and Blouse \n C. Romper \n D. Whatever is Clean \n Answer:")
+question2 = input("What are your go to shoes: \n A. Louboutins \n B. Nike \n C. Crocs \n D. UGGs \n Answer:")
+question3 = input("What’s favorite type of jeans: \n A. Skinny Jeans \n B. Bootcut Jeans \n C. Boyfriend Cut Jeans \n D. Jean Shorts \n Answer:")
 
 #Evaluate answers
-if question1 == "A":
-    if question2 == "A": 
-        if question3 =="A": 
-            if question4 == "A": 
-                if question5 == "A": 
-                    if question6 == "A":
-                        print("You're A Fashionista & Would Teach English!")
+def quiz():
+    if question1 == "A" and question2 == "A" and question3 == "A":
+        print("You Would Teach English!")
+    elif question1 == "B" and question2 == "A" and question3 == "A":
+        print("You Would Teach Psychology!")
+    elif question1 == "C" and question2 == "A" and question3 == "A":
+            print("You Would Teach US History!")
+    elif question1 == "D" and question2 == "A" and question3 == "A":
+        print("You Would Teach Biology!")
+    elif question1 == "A" and question2 == "B" and question3 == "A":
+            print("You Would Teach Economics!")
+    elif question1 == "A" and question2 == "C" and question3 == "A":
+            print("You Would Teach Health!")
+    elif question1 == "A" and question2 == "D" and question3 == "A":
+            print("You Would Teach Calculus!")
+    elif question1 == "A" and question2 == "A" and question3 == "B":
+            print("You Would Teach World History!")
+    elif question1 == "A"and question2 == "A" and question3 == "C":
+            print("You Would Teach Physical Education!")
+    elif question1 == "A" and question2 == "A"and question3 == "D":
+            print("You Would Teach Environmental Science!")
+    elif question1 == "A"and question2 == "B"and question3 == "B":
+            print("You Would Teach Statistics!")
+    elif question1 == "B"and question2 == "B"and question3 == "B":
+            print("You Would Teach Physics!")
+    elif question1 == "C"and question2 == "B"and question3 == "B":
+            print("You Would Teach Biology!")
+    elif question1 == "D" and question2 == "B"and question3 == "B":
+            print("You Would Teach English!")
+    elif question1 == "A" and question2 == "C" and question3 == "C":
+            print("You Would Teach Psychology!")
+    elif question1 == "B" and question2 == "C" and question3 == "C":
+            print("You Would Teach US History!")
+    elif question1 == "C" and question2 == "C" and question3 == "C":
+            print("You Would Teach Psychology!")
+    elif question1 == "D" and question2 == "C" and question3 == "C":
+            print("You Would Teach Economics!")
+    elif question1 == "A" and question2 == "D" and question3 == "D":
+            print("You Would Teach Health!")
+    elif question1 == "B" and question2 == "D" and question3 == "D":
+            print("You Would Teach Calculus!")
+    elif question1 == "C" and question2 == "D" and question3 == "D":
+            print("You Would Teach World History!")
+    elif question1 == "D" and question2 == "D" and question3 == "D":
+            print("You Would Teach Physical Education!")
+    elif question1 == "B" and question2 == "A" and question3 == "B":
+            print("You Would Teach Environmental Science!")
+    elif question1 == "B" and question2 == "C" and question3 == "B":
+                    print("You Would Teach Physics!")
+    elif question1 == "B" and question2 == "D" and question3 == "B":
+                    print("You Would Teach Psychology!")
+    elif question1 == "B" and question2 == "B" and question3 == "A":
+                    print("You Would Teach US History!")
+    elif question1 == "B" and question2 == "B" and question3 == "C":
+                    print("You Would Teach Biology!")
+    elif question1 == "B" and question2 == "B" and question3 == "D":
+                    print("You Would Teach Health!")
+    elif question1 == "C" and question2 == "A" and question3 == "B":
+                    print("You Would Teach Psychology!")
+    elif question1 == "D" and question2 == "A" and question3 == "B":
+                    print("You Would Teach Music!")
+    elif question1 == "B" and question2 == "A" and question3 == "D":
+                    print("You Would Teach Calculus!")
+    elif question1 == "C" and question2 == "A" and question3 == "D":
+                    print("You Would Teach Statistics!")
+    elif question1 == "D" and question2 == "A" and question3 == "D":
+                    print("You Would Teach US History!")
+    elif question1 == "B" and question2 == "A" and question3 == "C":
+                    print("You Would Teach World History!")
+    elif question1 == "C" and question2 == "A" and question3 == "C":
+                    print("You Would Teach Environmental Science!")
+    elif question1 == "D" and question2 == "A" and question3 == "C":
+                    print("You Would Teach Health!")
+    elif question1 == "C" and question2 == "B" and question3 == "A":
+                    print("You Would Teach Home Economics!")
+    elif question1 == "D" and question2 == "B" and question3 == "A":
+                    print("You Would Teach Music!")
+    elif question1 == "A" and question2 == "B" and question3 == "D":
+                    print("You Would Teach Home Economics!")
+    elif question1 == "C" and question2 == "B" and question3 == "D":
+                    print("You Would Teach Music!")
+    elif question1 == "D" and question2 == "B" and question3 == "D":
+                    print("You Would Teach Psychology!")
+    elif question1 == "A" and question2 == "B" and question3 == "C":
+                    print("You Would Teach Health!")
+    elif question1 == "C" and question2 == "B" and question3 == "C":
+                    print("You Would Teach Home Economics!")
+    elif question1 == "D" and question2 == "B" and question3 == "C":
+                    print("You Would Teach Music!")
+    elif question1 == "B" and question2 == "C" and question3 == "A":
+                    print("You Would Teach Home Economics!")
+    elif question1 == "C" and question2 == "C" and question3 == "A":
+                    print("You Would Teach Music!")
+    elif question1 == "D" and question2 == "C" and question3 == "A":
+                    print("You Would Teach US History!")
+    elif question1 == "A" and question2 == "C" and question3 == "B":
+                    print("You Would Teach World History!")
+    elif question1 == "C" and question2 == "C" and question3 == "B":
+                    print("You Would Teach Statistics!")
+    elif question1 == "D" and question2 == "C" and question3 == "B":
+                    print("You Would Teach Calculus!")
+    elif question1 == "A" and question2 == "C" and question3 == "D":
+                    print("You Would Teach Biology!")
+    elif question1 == "B" and question2 == "C" and question3 == "D":
+                    print("You Would Teach Physics!")
+    elif question1 == "C" and question2 == "C" and question3 == "D":
+                    print("You Would Teach Psychology!")
+    elif question1 == "D" and question2 == "C" and question3 == "D":
+                    print("You Would Teach US History!")
+    elif question1 == "B" and question2 == "D" and question3 == "A":
+                    print("You Would Teach World History!")
+    elif question1 == "C" and question2 == "D" and question3 == "A":
+                    print("You Would Teach English!")
+    elif question1 == "D" and question2 == "D" and question3 == "A":
+                    print("You Would Teach Economics!")
+    elif question1 == "A" and question2 == "D" and question3 == "B":
+                    print("You Would Teach Biology!")
+    elif question1 == "C" and question2 == "D" and question3 == "B":
+                    print("You Would Teach Physics!")
+    elif question1 == "D" and question2 == "D" and question3 == "B":
+                    print("You Would Teach Environmental Science!")
+    elif question1 == "A" and question2 == "D" and question3 == "C":
+                    print("You Would Teach Physical Education!")
+    elif question1 == "B" and question2 == "D" and question3 == "C":
+                    print("You Would Teach Health!")
+    elif question1 == "C" and question2 == "D" and question3 == "C":
+                    print("You Would Teach Music!")
+    elif question1 == "D" and question2 == "D" and question3 == "C":
+                    print("You Would Teach Home Economics!")
+quiz()
+
+answer = input("Thank you for taking quiz! Would you like to play again? \n")
+while answer.lower() == "yes":
+    print("Buzzfeed Quiz: Tell Us Your Style & We will Tell You What Subject You’ll Teach")
+    print("Please Put Your Answer As a Capital Letter")
+#Questions the quiz will ask: 
+    question1 = input("On a night out do you prefer: \n A. A Dress \n B. Jeans and Blouse \n C. Romper \n D. Whatever is Clean \n Answer:")
+    question2 = input("What are your go to shoes: \n A. Louboutins \n B. Nike \n C. Crocs \n D. UGGs \n Answer:")
+    question3 = input("What’s favorite type of jeans: \n A. Skinny Jeans \n B. Bootcut Jeans \n C. Boyfriend Cut Jeans \n D. Jean Shorts \n Answer:")
+    quiz()
+    answer = input("Thank you for taking quiz! Would you like to play again? \n" )
